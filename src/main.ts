e293081e9086fa51cf767b2eed55b5248c785344
Igor Ostapov <ostapovih@mydigicode.com>
2021-01-30 22:27:24 +0200

    add: swagger

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
 import { ValidationPipe } from './pipes/validation.pipe';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Championship')
+    .setVersion(process.version)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger_api', app, document);
+
   await app.listen(9000);
 }
 bootstrap();
